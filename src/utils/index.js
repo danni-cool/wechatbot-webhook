44550a030273a6dcc1b8b296ec8fcdf4f9202849
chentianyu <chentianyu@dxy.cn>
2023-09-21 16:19:29 +0800

    fix: 🐛 修复发送图片来自cloudflare 托管的url 返回 http状态码301图片发送不成功的问题

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,25 @@
-const { FileBox } = require('file-box') // bugfix: wechaty can not export FileBox, but wechaty is rely on file-box package
+const { FileBox } = require('file-box') // bugfix: wechaty can not export FileBox until v1.20.2 , so use the dependency of wechaty 
+const fetch = require('node-fetch-commonjs')
+
+const downloadImage = async imageUrl => {
+  try {
+    const response = await fetch(imageUrl);
+    if (response.ok) {
+      return await response.buffer();
+    }
+    return null;
+  } catch (error) {
+    console.error('Error downloading image:' + imageUrl, error);
+    return null;
+  }
+}
+
+//http://www.baidu.com/image.png?a=1 => image.png
+const getFileNameFromUrl = (url) => url.match(/.*\/([^/?]*)/)?.[1] || ''
+
+// bugfix: use `fileBox.fromUrl` api to get image is OK, but sometimes directly to get cloudflare img may return a 0 bites response.(when response is 301)
+const getMediaFromUrl = async url =>
+  FileBox.fromBuffer(await downloadImage(url), getFileNameFromUrl(url))
 
 const formatAndSendMsg = async function ({ type, content, msgInstance }) {
 
@@ -13,15 +34,15 @@ const formatAndSendMsg = async function ({ type, content, msgInstance }) {
       // 逗号分割的多张图的情况
       const imgArr = content.split(',')
       // 只有一张图
-      imgArr.length === 1 ?
-        await msgInstance.say(FileBox.fromUrl(imgArr[0])) :
-        // 多张图的情况
-        await (async () => {
-          for (let i = 0; i < imgArr.length; i++) {
-            await msgInstance.say(FileBox.fromUrl(imgArr[i]))
-          }
-        })()
+      if (imgArr.length === 1) {
+        await msgInstance.say(await getMediaFromUrl(content))
+        return true
+      }
 
+      // 多张图的情况
+      for (let i = 0; i < imgArr.length; i++) {
+        await msgInstance.say(await getMediaFromUrl(imgArr[i]))
+      }
       return true
   }
 }
